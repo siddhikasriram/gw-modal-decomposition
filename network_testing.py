17512d216c40f72c0bcd90bdc9fda6c7bcf614d4
siddhikasriram <86295696+siddhikasriram@users.noreply.github.com>
2022-02-03 11:23:43 +0530

    Update network_testing.py

--- network_testing.py
+++ network_testing.py
@@ -35,17 +35,19 @@ def labelling (fileName):
     labels = []
     labels.append(int(image[9]))
     labels.append(int(image[11]))
+
     if image[18] == 'p':
       labels.append(64 + int(image[19:21]))
-
     else:
       labels.append(64 + int("-"+image[19:21]))
 
     if image[22] == 'p':
       labels.append(64 + int("+"+image[23:25]))
-
     else:
       labels.append(64 + int("-"+image[23:25]))
+    
+    labels.append(float(image[13:17]))
+
     yParam.append(labels)
   yParam = np.array([np.array(x) for x in yParam])
   return yParam
@@ -54,123 +56,167 @@ fileName = os.listdir(pathName)
 y_test = labelling(fileName)
 
 yTest = model.predict(x_test)
-evaluate = model.evaluate(x_test, y_test)
-print(evaluate)
-print("Actual values\n", y_test)
-print("Predicted values\n", yTest)
+#evaluate = model.evaluate(x_test, y_test)
 
-yTest[:,0] = [round(x) for x in yTest[:,0]]
-yTest[:,1] = [round(x) for x in yTest[:,1]]
-yTest[:,2] = [round(x) for x in yTest[:,2]]
-yTest[:,3] = [round(x) for x in yTest[:,3]]
-
-actual = y_test.copy()
-predicted = yTest.copy()
-actual = [tuple(x[:2]) for x in actual]
-predicted = [tuple(x[:2]) for x in predicted]
-
-diff=[]
-for ind, act in enumerate(actual):
-  pred = predicted[ind]
-  z = [x - pred[i] for i, x in enumerate(act)]
-  diff.append(z)
-
-actual_keys = [tuple(i) for i in actual]
-
-mdiff = {}
-ndiff = {}
-for x in actual_keys:
-  mdiff[x]=[]
-  ndiff[x]=[]
+def show_output(y_test, yTest, fname, heading):
+  yTest[:,0] = [round(x) for x in yTest[:,0]]
+  yTest[:,1] = [round(x) for x in yTest[:,1]]
+  yTest[:,2] = [round(x) for x in yTest[:,2]]
+  yTest[:,3] = [round(x) for x in yTest[:,3]]
     
-for i, x in enumerate(actual):
-  if x in mdiff.keys():
-    mdiff[x].append(diff[i][0])
-    ndiff[x].append(diff[i][1])
 
-for k, v in mdiff.items():
-  mdiff[k]=mean(v)
-for k, v in ndiff.items():
-  ndiff[k]=mean(v)
+  actual = y_test.copy()
+  predicted = yTest.copy()
+  actual = [tuple(x[:2]) for x in actual]
+  predicted = [tuple(x[:2]) for x in predicted]
 
-fig = plt.figure(figsize=(13,20))
+  diff=[]
+  for ind, act in enumerate(actual):
+    pred = predicted[ind]
+    z = [x - pred[i] for i, x in enumerate(act)]
+    diff.append(z)
 
-plt.subplot(3,2,1)
-plt.title('TEM m - Mean deviation for all mode combinations')
-plt.xlabel('TEM m actual')
-plt.ylabel('TEM n actual')
-cm = plt.cm.get_cmap('autumn')
-actual = [tuple(x[:2]) for x in actual]
-c_list_m = []
+  actual_keys = [tuple (i) for i in actual]
 
-for i, a in enumerate(y_test[:,0]):
-  print(i)
-  p = yTest[i:, 0]
-  z = actual[i]
-  if z in mdiff.keys():
-    c_list_m.append(mdiff[z])
-sc = plt.scatter(y_test[:, 0], y_test[:, 1], c=c_list_m, cmap = cm)
-cbar1 = plt.colorbar(sc)
-cbar1.mappable.set_clim(vmin=-1,vmax=1)
+  mdiff = {}
+  ndiff = {}
+  for x in actual_keys:
+    mdiff[x]=[]
+    ndiff[x]=[]
 
-plt.subplot(3,2,2)
-plt.title('TEM m - Individual deviation from original value')
-plt.xlabel('m deviation')
-plt.ylabel('No. of Samples')
-plt.yscale('log')
-m_deviation = y_test[:, 0]-yTest[:, 0]
-plt.xlim(xmin=-5, xmax = 5)
-plt.ylim([1,10**5])
-#plt.hist(m_deviation)
-counts, bins, _ = plt.hist(m_deviation)
+  for i, x in enumerate(actual):
+    if x in mdiff.keys():
+        mdiff[x].append(diff[i][0])
+        ndiff[x].append(diff[i][1])
 
-for n, b in zip(counts, bins):
-  plt.gca().text(b + 0.05, n, int(n), rotation = 45)  # +0.1 to center text
+  for k, v in mdiff.items():
+    mdiff[k]=mean(v)
+  for k, v in ndiff.items():
+    ndiff[k]=mean(v)
 
-plt.subplot(3,2,3)
-plt.title('TEM n - Mean deviation for all mode combinations')
-plt.xlabel('TEM m actual')
-plt.ylabel('TEM n actual')
-cm = plt.cm.get_cmap('autumn')
-c_list_n = []
-for i, a in enumerate(y_test[:,0]):
-  p = yTest[i:, 0]
-  z = actual[i]
-  if z in ndiff.keys():
-    c_list_n.append(ndiff[z])
-sc = plt.scatter(y_test[:, 0], y_test[:, 1], c=c_list_n, cmap=cm)
-cbar2 = plt.colorbar(sc)
-cbar2.mappable.set_clim(vmin=-1,vmax=1)
+  plt.figure(figsize=(13,20))
 
-plt.subplot(3,2,4)
-plt.title('TEM n - Individual deviation from original value')
-plt.xlabel('n deviation')
-plt.ylabel('No. of Samples')
-plt.yscale('log')
-n_deviation = y_test[:, 1]-yTest[:, 1]
-plt.xlim(xmin=-5, xmax = 5)
-plt.ylim([1,10**5])
-#plt.hist(n_deviation)
-counts, bins, _ = plt.hist(n_deviation)
+  plt.suptitle(heading)
 
-for n, b in zip(counts, bins):
-  plt.gca().text(b+0.05, n, int(n), rotation = 45)  # +0.1 to center text
+  plt.subplot(3,2,1)
+  plt.title('TEM m - Mean deviation for all mode combinations')
+  plt.xlabel('TEM m actual')
+  plt.ylabel('TEM n actual')
+  cm = plt.cm.get_cmap('autumn')
+  actual = [tuple(x[:2]) for x in actual]
+  c_list_m = []
 
-plt.subplot(3,2,5)
-plt.title('X Offset')
-plt.xlabel('x off actual')
-plt.ylabel('x off predicted')
-plt.axis([30, 100, 30, 100])
-plt.scatter(y_test[:, 2], yTest[:, 2], color='green', marker='*')
+  for i, a in enumerate(y_test[:,0]):
+    print(i)
+    p = yTest[i:, 0]
+    z = actual[i]
+    if z in mdiff.keys():
+      c_list_m.append(mdiff[z])
+  sc = plt.scatter(y_test[:, 0], y_test[:, 1], c=c_list_m, cmap = cm)
+  cbar1 = plt.colorbar(sc)
+  cbar1.mappable.set_clim(vmin=-1,vmax=1)
 
-plt.subplot(3,2,6)
-plt.title('Y Offset')
-plt.xlabel('y off actual')
-plt.ylabel('y off predicted')
-plt.axis([30, 100, 30, 100])
-plt.scatter(y_test[:, 3], yTest[:, 3], color='green', marker='*')
+  plt.subplot(3,2,2)
+  plt.title('TEM m - Individual deviation from original value')
+  plt.xlabel('m deviation')
+  plt.ylabel('No. of Samples')
+  plt.yscale('log')
+  m_deviation = y_test[:, 0]-yTest[:, 0]
+  plt.xlim(xmin=-5, xmax = 5)
+  plt.ylim([1,10**5])
+  #plt.hist(m_deviation)
+  counts, bins, _ = plt.hist(m_deviation)
 
-plt.show()   
-plt.savefig("output13f.png")
+  for n, b in zip(counts, bins):
+    plt.gca().text(b + 0.05, n, int(n), rotation = 45)  # +0.1 to center text
+
+  plt.subplot(3,2,3)
+  plt.title('TEM n - Mean deviation for all mode combinations')
+  plt.xlabel('TEM m actual')
+  plt.ylabel('TEM n actual')
+  cm = plt.cm.get_cmap('autumn')
+  c_list_n = []
+  for i, a in enumerate(y_test[:,0]):
+    p = yTest[i:, 0]
+    z = actual[i]
+    if z in ndiff.keys():
+      c_list_n.append(ndiff[z])
+  sc = plt.scatter(y_test[:, 0], y_test[:, 1], c=c_list_n, cmap=cm)
+  cbar2 = plt.colorbar(sc)
+  cbar2.mappable.set_clim(vmin=-1,vmax=1)
+
+  plt.subplot(3,2,4)
+  plt.title('TEM n - Individual deviation from original value')
+  plt.xlabel('n deviation')
+  plt.ylabel('No. of Samples')
+  plt.yscale('log')
+  n_deviation = y_test[:, 1]-yTest[:, 1]
+  plt.xlim(xmin=-5, xmax = 5)
+  plt.ylim([1,10**5])
+  #plt.hist(n_deviation)
+  counts, bins, _ = plt.hist(n_deviation)
+
+  for n, b in zip(counts, bins):
+    plt.gca().text(b+0.05, n, int(n), rotation = 45)  # +0.1 to center text
+
+  plt.subplot(3,2,5)
+  plt.title('X Offset')
+  plt.xlabel('x off actual')
+  plt.ylabel('x off predicted')
+  plt.axis([30, 100, 30, 100])
+  plt.scatter(y_test[:, 2], yTest[:, 2], color='green', marker='*')
+
+  plt.subplot(3,2,6)
+  plt.title('Y Offset')
+  plt.xlabel('y off actual')
+  plt.ylabel('y off predicted')
+  plt.axis([30, 100, 30, 100])
+  plt.scatter(y_test[:, 3], yTest[:, 3], color='green', marker='*')
+
+
+  plt.show()   
+  #plt.savefig(fname)
+
+main_op = 'out.png'
+main_heading = 'Performance of the model for the entire test dataset'
+show_output(y_test, yTest, main_op, main_heading)
+
+low_noise_act = []
+low_noise_pred =[]
+med_noise_act =[]
+med_noise_pred =[]
+high_noise_act =[]
+high_noise_pred =[]
+for ind, act in enumerate(y_test):
+  pred = yTest[ind]
+  noise = y_test[ind,4]
+  if noise >= 0.05 and noise <= 0.3:
+    low_noise_act.append(y_test[ind,0:4])
+    low_noise_pred.append(yTest[ind, 0:4])
+  elif noise > 0.3 and noise <=0.6:
+    med_noise_act.append(y_test[ind,0:4])
+    med_noise_pred.append(yTest[ind, 0:4])
+  else:
+    high_noise_act.append(y_test[ind,0:4])
+    high_noise_pred.append(yTest[ind, 0:4])
+ 
+      
+low_noise_act = np.array([np.array(x) for x in low_noise_act])
+low_noise_pred = np.array([np.array(x) for x in low_noise_pred])
+low_noise_heading = 'Performance of the model when the noise is between 0.05 and 0.3'
+low_op = 'lowout.png'
+show_output(low_noise_act, low_noise_pred, low_op, low_noise_heading)
+
+med_noise_act = np.array([np.array(x) for x in med_noise_act])
+med_noise_pred = np.array([np.array(x) for x in med_noise_pred])
+med_noise_heading = 'Performance of the model when the noise is between 0.3 and 0.6'
+med_op = 'medout.png'
+show_output(med_noise_act, med_noise_pred, med_op, med_noise_heading)
+
+high_noise_act = np.array([np.array(x) for x in high_noise_act])
+high_noise_pred = np.array([np.array(x) for x in high_noise_pred])
+high_noise_heading = 'Performance of the model when the noise is between 0.6 and 0.9'
+high_op = 'highout.png'
+show_output(high_noise_act, high_noise_pred, high_op, high_noise_heading)
 
 
